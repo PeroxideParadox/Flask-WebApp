03ce7a0d7b2eafbd4e234796077ab8a6792d8468
PeroxideParadox <118070626+PeroxideParadox@users.noreply.github.com>
2024-05-05 14:49:00 +0530

    Updated Work

--- WebApp/website/auth.py
+++ WebApp/website/auth.py
@@ -1,19 +1,26 @@
 #as authentication will take place here
 
 from flask import Blueprint
-from flask import render_template
+from flask import render_template,request
 auth=Blueprint('auth',__name__)
 
 #for passing the values we will use the {{ inside this we can have python references}}
-@auth.route('/login')
+#by default it only accepts GET Requests 
+#Whenever Request is Inside a route we get all the information passed to the server here of the form that is being used 
+
+@auth.route('/login', methods=['POST','GET','post','get'])
 def login():
-    return render_template("login.html")
+    data=request.form
+    print(data)
+    return render_template("login.html",boolean=True)
 
 #will change this return with actual page with the Help of Jinga
-@auth.route('/signup')
+@auth.route('/signup',methods=['POST','GET'])
 def signup():
+    data=request.form
+    print(data)
     return render_template("signup.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout here</p>"
+    return render_template('login.html')
