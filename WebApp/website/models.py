ed71f9af3172198dc68f564e563a040c80c486d9
Bs <aromaticperoxide@gmail.com>
2024-05-07 18:04:51 +0530

    updated Databse

--- WebApp/website/models.py
+++ WebApp/website/models.py
@@ -7,14 +7,16 @@ from sqlalchemy import func
 #Defining the schema for the database 
 
 #Databse model is a layout or blueprint for an object that needs to be stored , so all user must look like this to ensure consistency
-class note(db.Model):
-    id=db.column(db.Integer(150),primary_key=True)
-    data=db.column(db.String(1000))
-    date=db.column(db.DateTime(timezone=True),default=func.now())
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(1000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-class user(db.Model,UserMixin):
-    id=db.column(db.Integer,primary_key=True)
-    email=db.column(db.String(100),unique=True)
-    password=db.column(db.String(50))
-    first_name=db.column(db.String(50))
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
